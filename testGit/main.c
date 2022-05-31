94641e54a3866459dabd348f988a17a9e950f22a
kimminsangA <71245477+kimminsangA@users.noreply.github.com>
2022-06-01 03:34:40 +0900

    Initial Commit

---
+++ testGit/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testGit
+//
+//  Created by 김민상 on 2022/06/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
